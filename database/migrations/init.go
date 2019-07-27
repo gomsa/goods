c35eb3ddad0fa1e99e9120ee95c712f56a85050c
bigrocs <bigrocs@qq.com>
2019-07-27 10:54:26 +0800

    自动提交 git 代码

--- database/migrations/init.go
+++ database/migrations/init.go
@@ -1,8 +1,8 @@
 package migrations
 
 import (
-	categoryPB "github.com/gomsa/goods/proto/category"
 	brandPB "github.com/gomsa/goods/proto/brand"
+	categoryPB "github.com/gomsa/goods/proto/category"
 	firmPB "github.com/gomsa/goods/proto/firm"
 	goodsPB "github.com/gomsa/goods/proto/goods"
 	taxcodePB "github.com/gomsa/goods/proto/taxcode"
@@ -29,14 +29,14 @@ func barcode() {
 			id int(36) NOT NULL COMMENT '商品条码',
 			goods_id varchar(36) DEFAULT NULL COMMENT '商品UUID',
 			stock_id varchar(36) DEFAULT NULL COMMENT '库存自动UUID(多条码可以使用一个库存[禁止不同价格使用同一库存UUID])',
-			price int(32) DEFAULT NULL COMMENT '价格',
-			width int(32) DEFAULT NULL COMMENT '宽(毫米)',
-			height int(32) DEFAULT NULL COMMENT '高(毫米)',
-			depth int(32) DEFAULT NULL COMMENT '深(毫米)',
+			price int(32) DEFAULT 0 COMMENT '价格',
+			width int(32) DEFAULT 0 COMMENT '宽(毫米)',
+			height int(32) DEFAULT 0 COMMENT '高(毫米)',
+			depth int(32) DEFAULT 0 COMMENT '深(毫米)',
 			unit varchar(16) DEFAULT NULL COMMENT '单位(件、个、斤...)',
 			spec varchar(64) DEFAULT NULL COMMENT '规格',
-			grossweight int(64) DEFAULT NULL COMMENT '总重(克)',
-			netweight int(64) DEFAULT NULL COMMENT '净重(克)',
+			grossweight int(64) DEFAULT 0 COMMENT '总重(克)',
+			netweight int(64) DEFAULT 0 COMMENT '净重(克)',
 			img varchar(128) DEFAULT NULL COMMENT '主图',
 			barcode_img varchar(128) DEFAULT NULL COMMENT '条码图',
 			created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -58,15 +58,15 @@ func goods() {
 			id varchar(36) NOT NULL COMMENT '商品ID(UUID)',
 			name varchar(64) DEFAULT NULL COMMENT '商品名称',
 			eng_name varchar(64) DEFAULT NULL COMMENT '英文名称',
-			desc varchar(255) DEFAULT NULL COMMENT '商品描述',
-			cess varchar(8) DEFAULT NULL COMMENT '税率',
-			status  int(11) DEFAULT NULL COMMENT '商品状态(下架-1、上架1、线上2、外卖3)',
-			brand_id int(11) DEFAULT NULL COMMENT '品牌ID',
-			category_id int(11) DEFAULT NULL COMMENT '商品分类ID',
-			department_id int(11) DEFAULT NULL COMMENT '部门分类ID',
-			firm_id int(11) DEFAULT NULL COMMENT '所属公司ID',
-			unspsc_id int(11) DEFAULT NULL COMMENT '商品及服务编码',
-			taxcode_id int(11) DEFAULT NULL COMMENT '税收分类编码',
+			description varchar(255) DEFAULT NULL COMMENT '商品描述',
+			cess int(8) DEFAULT 0 COMMENT '税率',
+			status  int(11) DEFAULT '-1' COMMENT '商品状态(下架-1、上架1、线上2、外卖3)',
+			brand_id int(11) DEFAULT 0 COMMENT '品牌ID',
+			category_id int(11) DEFAULT 0 COMMENT '商品分类ID',
+			department_id int(11) DEFAULT 0 COMMENT '部门分类ID',
+			firm_id int(11) DEFAULT 0 COMMENT '所属公司ID',
+			unspsc_id int(11) DEFAULT 0 COMMENT '商品及服务编码',
+			taxcode_id int(11) DEFAULT 0 COMMENT '税收分类编码',
 			created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
 			updated_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
 			xxx_unrecognized varbinary(255) DEFAULT NULL,
@@ -86,7 +86,7 @@ func brand() {
 			id int(11) unsigned NOT NULL AUTO_INCREMENT COMMENT '品牌ID',
 			name varchar(36) DEFAULT NULL COMMENT '品牌名称',
 			logo varchar(64) DEFAULT NULL COMMENT '品牌LOGO',
-			desc varchar(64) DEFAULT NULL COMMENT '品牌描述',
+			description varchar(64) DEFAULT NULL COMMENT '品牌描述',
 			created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
 			updated_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
 			xxx_unrecognized varbinary(255) DEFAULT NULL,
@@ -102,9 +102,9 @@ func category() {
 	category := &categoryPB.Category{}
 	if !db.DB.HasTable(&category) {
 		db.DB.Exec(`
-			CREATE TABLE categorys (
+			CREATE TABLE categories (
 			id int(11) unsigned NOT NULL AUTO_INCREMENT COMMENT '分类ID',
-			parent int(11) DEFAULT NULL COMMENT '上级分类ID,
+			parent int(11) DEFAULT 0 COMMENT '上级分类ID',
 			name varchar(36) DEFAULT NULL COMMENT '分类名称',
 			created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
 			updated_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
@@ -142,7 +142,7 @@ func unspsc() {
 		db.DB.Exec(`
 			CREATE TABLE unspscs (
 			id int(11) unsigned NOT NULL AUTO_INCREMENT COMMENT '分类ID',
-			parent int(11) DEFAULT NULL COMMENT '上级分类ID,
+			parent int(11) DEFAULT 0 COMMENT '上级分类ID',
 			name varchar(36) DEFAULT NULL COMMENT '分类名称',
 			created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
 			updated_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
@@ -161,12 +161,12 @@ func taxcode() {
 		db.DB.Exec(`
 			CREATE TABLE taxcodes (
 			id int(32) unsigned NOT NULL AUTO_INCREMENT COMMENT '分类ID',
-			parent int(32) DEFAULT NULL COMMENT '上级分类ID,
-			is_parent int(1) DEFAULT NULL COMMENT '是否是上级',
+			parent int(32) DEFAULT 0 COMMENT '上级分类ID',
+			is_parent int(1) DEFAULT 0 COMMENT '是否是上级',
 			cess varchar(8) DEFAULT NULL COMMENT '税率',
 			duty_free varchar(64) DEFAULT NULL COMMENT '税收减免情况',
 			duty_free_desc varchar(255) DEFAULT NULL COMMENT '税收减免情况详情',
-			code int(32) DEFAULT NULL COMMENT '编码',
+			code int(32) DEFAULT 0 COMMENT '编码',
 			name varchar(64) DEFAULT NULL COMMENT '名称',
 			instruction varchar(255) DEFAULT NULL COMMENT '说明', 
 			keyword varchar(128) DEFAULT NULL COMMENT '关键词', 
--- main_test.go
+++ main_test.go
@@ -1,11 +1,31 @@
 package main
 
 import (
-	"context"
 	"fmt"
 	"testing"
+
+	goodsPB "github.com/gomsa/goods/proto/goods"
 )
 
-func TestGoods(t *testing.T) {
+func TestAddGoods(t *testing.T) {
+	goods := goodsPB.Good{
+		Name:        `测试商品`,
+		EngName:     `goods`,
+		Description: `描述`,
+		Cess:        0,
+		Barcodes: []*goodsPB.Barcode{
+			{
+				Id:      6152340211,
+				StockId: `asdas1s23123`,
+				Price:   1000,
+			},
+			{
+				Id:      6152340212,
+				StockId: `asdas1s23123`,
+				Price:   1000,
+			},
+		},
+	}
+	fmt.Println(goods)
 	t.Log(t)
 }
